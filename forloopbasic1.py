b276b6e5847cc5886ddf1de135758a23c23229e7
whkessler <82409360+whkessler@users.noreply.github.com>
2021-05-04 11:30:25 -0500

    first commit

---
+++ forloopbasic1.py
@@ -0,0 +1,34 @@
+for i in range(0,151):
+    print(i)
+for i in range(5,1001,5):
+    print(i)
+for i in range(1,101,1):
+    if(i % 5 == 0):
+        print("coding")
+    if(i % 10 == 0):
+        print("coding dojo")
+    else:
+        print(i)
+i=0
+sum=0
+while i <= 500000:
+    if i % 2 !=0:
+        sum=sum+i 
+    i=i+1
+print(sum)        
+for i in range(2018,0,-4):
+    print(i)
+
+lownum = 2
+highnum = 9
+mult = 3
+for x in range(lownum,highnum):
+    if x % mult == 0:
+        print(x)
+    
+
+
+
+
+
+
